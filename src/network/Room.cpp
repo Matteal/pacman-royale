5b155cad6ebb4b394d57ef0cea6cf08ecf11c0d6
Bastien Ruivo <Bastien.ruivo@gmail.com>
2021-05-07 22:38:25 +0200

    d

--- src/network/Room.cpp
+++ src/network/Room.cpp
@@ -189,7 +189,8 @@ void Room::mainloop()
 					
 				i++;
 			}
-			if(info.size() == 2)
+			cout<<"index = "<<info[i] - 48<<" dir = "<<info[i] - 48<<endl;
+			if(info.size() > 1)
 			{
 				cout<<"index = "<<info[i] - 48<<" dir = "<<info[i] - 48<<endl;
 				pacList->at(info.at(1) - 48)->_dirNext = (direction)(info.at(0) - 48);
